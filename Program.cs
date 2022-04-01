c9db13b45450e39f5ba21384afa91b590033c645
Bryan Mayer <mayerbryan@mail.com>
2022-03-31 22:21:34 -0300

    articles function update

--- Program.cs
+++ Program.cs
@@ -12,6 +12,17 @@ namespace CoursesWebSite
         {
             var articles = new List<Article>();
             articles.Add(new Article("Article about OOP", "object-oriented"));
+            articles.Add(new Article("Article about C#", "object-oriented"));
+            articles.Add(new Article("Article about .NET", "object-oriented"));
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.Id);
+                Console.WriteLine(article.Title);
+                Console.WriteLine(article.Url);
+            }
         }
+
+
     }
 }
